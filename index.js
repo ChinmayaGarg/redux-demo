45993cdbdfdc7226a3e5ed26689f9f28a765991d
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-02-26 21:45:21 +0530

    Learning: Action Struct (type &/or anyother object ot key-value pair)
    We are not restricted to just have a type property, other than type the structure of an action object is completely upto developer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const BUY_CAKE = 'BUY_CAKE';
 
-// Action is an object with type property.
+// Action is an object with type property. We are not restricted to just have a type property, other than type the
+// structure of an action object is completely upto developer
 {
-  type: BUY_CAKE;
+  type: BUY_CAKE,
+  info: 'First redux action',
 }
