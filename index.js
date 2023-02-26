dffad623548bbf4fd30dffd452a236ddae1d0adb
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-02-26 22:00:53 +0530

    Learning: Action Creator
    With redux we also go an extra mile to implement action creator. Action creator simply creates an action. In terms of code, an action creator is a function that returns an action.

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const BUY_CAKE = 'BUY_CAKE';
 
 // Action is an object with type property. We are not restricted to just have a type property, other than type the
 // structure of an action object is completely upto developer
-{
-  type: BUY_CAKE,
-  info: 'First redux action',
+// {
+//   type: BUY_CAKE,
+//   info: 'First redux action',
+// }
+
+// With redux we also go an extra mile to implement action creator. Action creator simply creates an action.
+// In terms of code, an action creator is a function that returns an action.
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action'
+  };
 }
